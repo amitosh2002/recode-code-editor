9f9aa10f5fb768636d9bcc4d9652690df8fab4e4
Amitosh Kumar <amitoshroy2002@gmail.com>
2025-04-12 23:26:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,37 @@
-import bodyParser from "body-parser";
-const app = express();
 import express from "express";
-import { Router } from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import route from "./Router/questionRoutes.js";
 import userRoute from "./Router/userRoutes.js";
-import dotenv from "dotenv";
 import TestResultRoute from "./Router/testResultRoutes.js";
-
 import TestRoute from "./Router/testRoute.js";
+
+dotenv.config();
+
+const app = express();
 app.use(bodyParser.json());
 
 const allowedOrigins = [
-  "http://localhost:5173",                  // Local dev
-  "https://recode-code-editor.onrender.com" // Production
+  "http://localhost:5173",                  
+  "https://recode-code-editor.onrender.com"
 ];
+
 app.use(
-  cors(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
   })
 );
-dotenv.config();
+
 const PORT = process.env.PORT || 6001;
 const URI = process.env.MONGO_URI;
 
